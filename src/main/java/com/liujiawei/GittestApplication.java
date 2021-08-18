531207a025223060f6d00ef2b2cddf3013f6302c
liubasui <910998629@qq.com>
2021-08-18 21:29:54 +0800

    3try

---
+++ src/main/java/com/liujiawei/GittestApplication.java
@@ -0,0 +1,13 @@
+package com.liujiawei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestApplication.class, args);
+    }
+
+}
